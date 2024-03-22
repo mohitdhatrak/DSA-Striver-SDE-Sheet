5427033b9ded12490b45a1f1404c9bffcd23dada
mohitdhatrak <msdhatrak25@gmail.com>
2024-03-22 12:34:09 +0530

    problem 1

---
+++ day1-arrays/set-matrix-zeroes/brute.cpp
@@ -0,0 +1,49 @@
+// time complexity = O(n^3)
+
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        set<int> rows, columns;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rows.insert(i);
+                    columns.insert(j);
+                }
+            }
+        }
+
+        for (int row : rows) {
+            for (int j = 0; j < n; j++) {
+                matrix[row][j] = 0;
+            }
+        }
+
+        for (int column : columns) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][column] = 0;
+            }
+        }
+    }
+};
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+
+    Solution s;
+    vector<vector<int>> matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    s.setZeroes(matrix);
+}
